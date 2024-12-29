8e8fd83f4bf725d2209caf02efdc154b086dc187
Nader Quwatli <nader.qas@gmail.com>
2024-12-29 10:22:38 +0300

    icon change

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,18 +7,18 @@ import { motion } from "framer-motion";
 const Hero = () => {
   const variants = {
     hidden: { x: 0, y: 70, opacity: 0.25 },
-    visible: { x: 0, y: -10, opacity: 1, transition: { delay: 0.05 } },
+    visible: { x: 0, y: -70, opacity: 1, transition: { delay: 0.05 } },
   };
   return (
     <>
       <div className="hero-section">
         <div className="hero-container">
           <div className="object-cover">
-            <Image src={Airpods} alt="banner" height="100" width="1400" />
+            <Image src={Airpods} alt="banner" height="100" width="1200" />
           </div>
           <div className="hero-airpods">
             <motion.div initial="hidden" animate="visible" variants={variants}>
-              <Image src={HeroAirpods} alt="banner" height="100" width="700" />
+              <Image src={HeroAirpods} alt="banner" width="400" height="50"  />
             </motion.div>
           </div>
         </div>
